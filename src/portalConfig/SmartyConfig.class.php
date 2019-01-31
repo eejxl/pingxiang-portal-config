0052a7d0c0823938215525d41852eddaa970279c
WooKy <939410146@qq.com>
2019-01-31 13:55:58 +0800

    #smartyConfig

---
+++ src/portalConfig/SmartyConfig.class.php
@@ -0,0 +1,18 @@
+<?php
+namespace portalConfig;
+
+use Marmot\Core;
+
+class SmartyConfig
+{
+    public static function configDir()
+    {
+        $dirArray = array(
+            S_ROOT.'vendor/eejxl/pingxiang-portal-config/src/portalConfig/px',
+            S_ROOT.'vendor/eejxl/pingxiang-portal-config/src/portalConfig/common',
+            S_ROOT.'vendor/eejxl/pingxiang-portal-config/src/portalConfig/',
+        );
+
+        return $dirArray;
+    }
+}
